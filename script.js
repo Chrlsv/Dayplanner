437ba48b4ecbdb5cd976e4615b0a0d4ebd5918a8
charl <charlesgsv@gmail.com>
2019-10-28 00:09:27 -0400

    update on loop

--- script.js
+++ script.js
@@ -2,27 +2,32 @@ $(document).ready(function(){
 
     var m = moment();
     var currentDate = m.format("MMMM Do YYYY");
-    var currentHour = moment().hours();
+    var currentHour = moment().format("HH"); //moment.format("HH")
 
     $("#currentDay").text("Today's Date: " + currentDate);
 
+    
     $(".saveBtn").on("click",function(){
         console.log("button");
         var eventInput = $("#description").val();
         localStorage.setItem($((this).attr("data-value")), eventInput);
     });
 
+    //creating a loop to fulfill condition
     function timeStyle(){
-        if($(".row").attr("data-value") < currentHour) {
+        console.log($(".row"),currentHour);
+        var rows = $(".row");
+        for(var i = 0; i < rows.length; i++){
+        if($(".row").attr("data-value") > currentHour) {
             $(".row").attr("class", "row future");
         }
         else if($(".row").attr("data-value") === currentHour) {
             $(".row").attr("class", "row present");
-        }
-        else($(".row").attr("data-value") < currentHour); {
+        }else{
+            //$(".row").attr("data-value") < currentHour); {
             $(".row").attr("class", "row past");
         }
-        
+    }
     };
     timeStyle();
 })
